f8cb375c575ac48b7b6f2ec4c66e9bc68106ed21
Mikkel Hansen <mikemiha90@gmail.com>
2021-03-26 13:03:23 +0100

    Added simple Footer and Header components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>React Dashboard footer</h2>
+        </footer>
+    );
+};
+
+export default Footer;
