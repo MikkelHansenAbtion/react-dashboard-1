b8706943ee24c66e27d2dcba55124c49e552baba
Mikkel Hansen <mikemiha90@gmail.com>
2021-03-26 14:27:16 +0100

    Added reset styling and simple global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import "./assets/reset.scss";
+import "./assets/global.scss";
+
 function App() {
     return (
-        <div>
+        <>
             <Header />
             <Footer />
-        </div>
+        </>
     );
 }
 
