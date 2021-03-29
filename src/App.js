9c0d88646408ad7b678c7df0661dfb96c3ec8d5a
Mikkel Hansen <meh@abtion.com>
2021-03-29 12:59:00 +0200

    Added delete-entry functionality

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Matrix from "./components/Matrix";
-import EntryForm from "./components/EntryForm";
-import { useState } from "react";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Matrix from "./components/Matrix"
+import EntryForm from "./components/EntryForm"
+import { useState } from "react"
 
-import "./assets/reset.scss";
-import "./assets/global.scss";
+import "./assets/reset.scss"
+import "./assets/global.scss"
 
 function App() {
-    const [entries, setEntries] = useState([
-        {
-            id: 1,
-            text: "One",
-            time: "Yesterday",
-        },
-        {
-            id: 2,
-            text: "Two",
-            time: "Today",
-        },
-        {
-            id: 3,
-            text: "Three",
-            time: "Tomorrow",
-        },
-    ]);
+  const [entries, setEntries] = useState([
+    {
+      id: 1,
+      text: "One",
+      time: "Yesterday",
+    },
+    {
+      id: 2,
+      text: "Two",
+      time: "Today",
+    },
+    {
+      id: 3,
+      text: "Three",
+      time: "Tomorrow",
+    },
+  ])
 
-    const AddMatrixEntry = (entry) => {
-        const id = Math.floor(Math.random() * 100) + 1;
-        const newEntry = {
-            id: id,
-            ...entry,
-        };
-        console.log(id);
-        setEntries([...entries, newEntry]);
-    };
+  const AddMatrixEntry = (entry) => {
+    const id = Math.floor(Math.random() * 100) + 1
+    const newEntry = {
+      id: id,
+      ...entry,
+    }
+    console.log(id)
+    setEntries([...entries, newEntry])
+  }
 
-    // const DeleteMatrixEntry = (id) => {
-    //     console.log(id);
-    // };
+  const DeleteMatrixEntry = (id) => {
+    setEntries(entries.filter((entry) => entry.id !== id))
+  }
 
-    return (
-        <div className='content'>
-            <Header />
-            <EntryForm onAdd={AddMatrixEntry} />
-            <Matrix rows={entries} />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="content">
+      <Header />
+      <EntryForm onAdd={AddMatrixEntry} />
+      <Matrix rows={entries} onDelete={DeleteMatrixEntry} />
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Matrix/Matrix.jsx
+++ src/components/Matrix/Matrix.jsx
@@ -1,26 +1,27 @@
-import "./Matrix.scss";
+import "./Matrix.scss"
 
-const Matrix = ({ rows }) => {
-    return (
-        <table className='matrix'>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Text</th>
-                    <th>Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows.map((entry) => (
-                    <tr key={("key_", entry.id)}>
-                        <td>{entry.id}</td>
-                        <td>{entry.text}</td>
-                        <td>{entry.time}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-};
+const Matrix = ({ rows, onDelete }) => {
+  return (
+    <table className="matrix">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Text</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((entry) => (
+          <tr key={("key_", entry.id)}>
+            <td>{entry.id}</td>
+            <td>{entry.text}</td>
+            <td>{entry.time}</td>
+            <td onClick={() => onDelete(entry.id)}>DELETE</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
-export default Matrix;
+export default Matrix
