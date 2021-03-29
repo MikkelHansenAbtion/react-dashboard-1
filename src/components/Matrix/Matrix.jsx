4775db75784fca0674b1e5ab1b8a9acdfa17ac8b
Mikkel Hansen <meh@abtion.com>
2021-03-29 14:16:23 +0200

    Added NO ENTRIES message when no entries found
    Also made entry deletion a clickable cross

--- src/components/Matrix/Matrix.jsx
+++ src/components/Matrix/Matrix.jsx
@@ -8,17 +8,26 @@ const Matrix = ({ rows, onDelete }) => {
           <th>Id</th>
           <th>Text</th>
           <th>Time</th>
+          <th>DELETE</th>
         </tr>
       </thead>
       <tbody>
-        {rows.map((entry) => (
-          <tr key={("key_", entry.id)}>
-            <td>{entry.id}</td>
-            <td>{entry.text}</td>
-            <td>{entry.time}</td>
-            <td onClick={() => onDelete(entry.id)}>DELETE</td>
+        {rows.length > 0 ? (
+          rows.map((entry) => (
+            <tr key={("tr_", entry.id)}>
+              <td>{entry.id}</td>
+              <td>{entry.text}</td>
+              <td>{entry.time}</td>
+              <td onClick={() => onDelete(entry.id)}>
+                <i className="clickable">&#10006;</i>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr key="tr_noresult">
+            <td colspan="4">NO ENTRIES</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   )
