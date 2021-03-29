9abedd9c47c97a99806247cec9a8ed37b949aca2
Mikkel Hansen <meh@abtion.com>
2021-03-29 13:37:28 +0200

    Added json-server for debugging

--- src/App.js
+++ src/App.js
@@ -2,49 +2,49 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Matrix from "./components/Matrix"
 import EntryForm from "./components/EntryForm"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import "./assets/reset.scss"
 import "./assets/global.scss"
 
 function App() {
-  const [entries, setEntries] = useState([
-    {
-      id: 1,
-      text: "One",
-      time: "Yesterday",
-    },
-    {
-      id: 2,
-      text: "Two",
-      time: "Today",
-    },
-    {
-      id: 3,
-      text: "Three",
-      time: "Tomorrow",
-    },
-  ])
+  const [entries, setEntries] = useState([])
 
-  const AddMatrixEntry = (entry) => {
-    const id = Math.floor(Math.random() * 100) + 1
-    const newEntry = {
-      id: id,
-      ...entry,
+  useEffect(() => {
+    const getEntries = async () => {
+      const entriesFromServer = await fetchEntries()
+      setEntries(entriesFromServer)
     }
-    console.log(id)
-    setEntries([...entries, newEntry])
+    getEntries()
+  }, [])
+
+  const fetchEntries = async () => {
+    const result = await fetch("http://localhost:5000/entries")
+    const data = await result.json()
+    return data
   }
 
-  const DeleteMatrixEntry = (id) => {
+  const addMatrixEntry = async (entry) => {
+    const result = await fetch("http://localhost:5000/entries", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(entry),
+    })
+    const data = await result.json()
+    setEntries([...entries, data])
+  }
+
+  const deleteMatrixEntry = async (id) => {
+    await fetch(`http://localhost:5000/entries/${id}`, { method: "DELETE" })
+
     setEntries(entries.filter((entry) => entry.id !== id))
   }
 
   return (
     <div className="content">
       <Header />
-      <EntryForm onAdd={AddMatrixEntry} />
-      <Matrix rows={entries} onDelete={DeleteMatrixEntry} />
+      <EntryForm onAdd={addMatrixEntry} />
+      <Matrix rows={entries} onDelete={deleteMatrixEntry} />
       <Footer />
     </div>
   )
